18e30e1cd32b34225a2b6712d6756f23132a60b0
eubest <eubest00@gmail.com>
2023-09-16 21:51:09 +0300

    config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   eslint: {
     // Disabling on production builds because we're running checks on PRs via GitHub Actions.
-    ignoreDuringBuilds: false,
+    ignoreDuringBuilds: true,
   },
   experimental: {
     serverActions: true,
