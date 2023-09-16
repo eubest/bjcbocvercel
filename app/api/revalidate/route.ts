f25c63bb269acc255d33bdeaa5287a943a8facb2
eubest <eubest00@gmail.com>
2023-09-16 19:54:36 +0300

    awayt req async to api/tevalidate/route.ts

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,5 +4,5 @@ import { NextRequest } from 'next/server';
 export const runtime = 'edge';
 
 export async function POST(req: NextRequest): Promise<Response> {
-  return revalidate(req);
+  return await revalidate(req);
 }
